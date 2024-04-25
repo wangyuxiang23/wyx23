56c840fc0f2ad72d5e445b9fe30693cd9a9df508
wangyuxiang23 <167405703+wangyuxiang23@users.noreply.github.com>
2024-04-25 14:32:22 +0800

    Update GitHubwyx.java

--- GitHubwyx.java
+++ GitHubwyx.java
@@ -1,5 +1,5 @@
-public class helloWorld{
+public class HelloWorld{
   public static void main(String[] args){
-    System.out.println("hello  World!")
+    System.out.println("hello  World!");
   }
 }
